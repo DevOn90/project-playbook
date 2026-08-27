e371a66b46900f9b34d343530b0896331c86cabf
me <me@example.com>
2026-08-27 10:13:50 +0200

    feat: preliminary release

---
+++ modules/angular/bin/main-angular.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# ============================================================
+# Purpose: TBD
+# Logic:
+#        1. Set Global Variables
+#        2. Create logging scripts
+# ============================================================
+
+set -euo pipefail
+
+# ============================================================
+# 1. Set Global Variables
+# ============================================================
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+MODULE_ROOT="$(cd -- "$SCRIPT_DIR/../" && pwd)"
+PROJECT_ROOT="$(cd -- "$SCRIPT_DIR/../../../" && pwd)"
+SCRIPT_NAME="$(basename -- "${BASH_SOURCE[0]}")"
+
+TS=$(date +%Y-%m-%dT%H:%M:%S)
+
+# ============================================================
+# 2. Create logging scripts
+# ============================================================
+
+log_info() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;34m[INFO][$TS][$script_name] \033[0m $1"
+}
+
+log_error() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;31m[ERROR][$TS][$script_name] \033[0m $1"
+} 
+
+log_warning() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;33m[WARNING][$TS][$script_name] \033[0m $1"
+}
+
+log_debug() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;36m[DEBUG][$TS][$script_name] \033[0m $1"
+}
+
+log_info "I am child process at $MODULE_ROOT/bin/main-angular.sh"
