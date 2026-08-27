31beae1ea756b5bc04a4d4946c05947ded4d4b8d
me <me@example.com>
2026-08-27 10:03:26 +0200

    fix: rename files fix: comment out script executable parts to enable test of main-root.sh orchestration

---
+++ bootstrap/main-bootstrap.sh
@@ -0,0 +1,299 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# ---------------------------------------------------------
+# Purpose: This script is designed to automate the setup of a new project environment. 
+#
+# Logic:
+#        1. Set Global Variables
+#        2. Create logging scripts
+#        3. Prompt the user for a project name.
+#        4. Prompt the user for a path where they want to create the project directory.
+#        5. Let user confirm existence of GH repo for the project name. [n/Y]
+#        6. If yes, clone the repo; else, ask to create a new repo and ask for repo existence again.
+#        7. Change directory to the project directory.
+#        8. Setup project environment.
+#        8.1 Setup Git conf user.name and user.email for the new project.
+#        8.2 Validate Git LFS installation and initialization in the repository.
+#        8.3 Validate local installation of uv (Astral).
+#        8.4 Set custom git config hookpath
+#        9. Main function
+# ---------------------------------------------------------
+
+# ---------------------------------------------------------
+# 1. Global Variables
+# ---------------------------------------------------------
+PROJECT_NAME=""
+TS=$(date +%Y-%m-%dT%H:%M:%S)
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_NAME="$(basename -- "${BASH_SOURCE[0]}")"
+GIT_HOOK_PATH=".githooks"
+
+GH_USERNAME=""
+GH_CONF_USER_NAME=""
+GH_CONF_USER_EMAIL=""
+
+# ---------------------------------------------------------
+# 2. Logging Scripts
+# ---------------------------------------------------------
+
+log_info() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;34m[INFO][$TS][$script_name] \033[0m $1"
+}
+
+log_error() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;31m[ERROR][$TS][$script_name] \033[0m $1"
+} 
+
+log_warning() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;33m[WARNING][$TS][$script_name] \033[0m $1"
+}
+
+log_debug() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;36m[DEBUG][$TS][$script_name] \033[0m $1"
+}
+
+log_info "I am child process at $SCRIPT_DIR/main-bootstrap.sh"
+
+# # ---------------------------------------------------------
+# # 3. Function to prompt the user for a project name
+# # ---------------------------------------------------------
+# prompt_project_name() {
+#   while true; do
+#     echo ""
+#     read -p "Enter the project name: " PROJECT_NAME
+
+#     # Validate empty input
+#     if [[ -z "$PROJECT_NAME" ]]; then
+#       log_error "Project name cannot be empty. Please try again."
+#       continue
+#     fi
+
+#     break
+#   done
+# }
+
+# # ---------------------------------------------------------
+# # 4. Function to prompt the user for a project path
+# # ---------------------------------------------------------
+# prompt_project_path() {
+#   echo ""
+#   read -p $'Provide the path where you want to create\nthe project directory (e.g. /home/user/Desktop ): ' PROJECT_PATH
+#   PROJECT_PATH=${PROJECT_PATH:-$(pwd)}
+#   echo ""
+  
+#   # Validate the provided path
+#   if [[ ! -d "$PROJECT_PATH" ]]; then
+#     log_error "The provided path '$PROJECT_PATH' does not exist. Please provide a valid directory path."
+#     exit 1
+#   fi
+  
+# }
+
+# # ---------------------------------------------------------
+# # 5. Function to prompt the user for GitHub username
+# # ---------------------------------------------------------
+# prompt_github_username() {
+#   echo ""
+#   read -p "Enter your GitHub username (default is 'gh-username'): " GH_USERNAME
+#   GH_USERNAME=${GH_USERNAME:-gh-username}
+#   echo ""
+# } 
+
+# # ---------------------------------------------------------
+# # 6. Function to confirm the existence of a GitHub repository for the project name
+# # ---------------------------------------------------------
+# confirm_github_repo_existence() {
+#   echo ""
+#   read -p "Does a GitHub repository exist for the project '$PROJECT_NAME'? [n/Y]: " REPO_EXISTS
+#   echo ""
+#   REPO_EXISTS=${REPO_EXISTS:-Y}
+
+#   if [[ "$REPO_EXISTS" =~ ^[Yy]$ ]]; then
+
+#     # Validate the existence of the GitHub repository using the GitHub API
+#     response=$(curl -s -o /dev/null -w "%{http_code}" \
+#     "https://api.github.com/repos/$GH_USERNAME/$PROJECT_NAME")
+    
+#     if [[ "$response" == "200" ]]; then
+#       log_info "GitHub repository '$GH_USERNAME/$PROJECT_NAME' exists."
+#     else
+#       log_error "GitHub repository '$GH_USERNAME/$PROJECT_NAME' does not exist. Please create one and try again."
+#       exit 1
+#     fi
+
+#   else
+#     log_info "GitHub repository does not exist for the project '$PROJECT_NAME'. Please create one and try again."
+#     exit 1
+#   fi
+# }
+
+# # ---------------------------------------------------------
+# # 7. Function to clone the GitHub repository
+# # ---------------------------------------------------------
+# clone_github_repo() {
+#   echo ""
+#   log_info "Cloning the GitHub repository '$GH_USERNAME/$PROJECT_NAME'..."
+#   git clone "https://github.com/$GH_USERNAME/$PROJECT_NAME.git" "$PROJECT_PATH/$PROJECT_NAME"
+# }
+
+# # ---------------------------------------------------------
+# # 8. Function to prepare the project environment using the bootstrap script
+# # ---------------------------------------------------------
+
+# # 8.1 Setup Git configuration for the new project
+
+# prepare_project_environment() {
+#   echo ""
+#   log_info "Preparing the project environment..."
+  
+#   # ---------------------------------------------------------
+#   # 8.2 Setup Git configuration for the new project
+#   # ---------------------------------------------------------
+#   log_info "Setting up Git configuration for the new project..."
+
+#   echo ""
+#   read -p "Enter your Git user.name for this project (default is 'me'): " GH_CONF_USER_NAME
+#   echo ""
+#   read -p "Enter your Git user.email for this project (default is 'me@example.com'): " GH_CONF_USER_EMAIL
+#   echo ""
+#   GH_CONF_USER_NAME=${GH_CONF_USER_NAME:-me}
+#   GH_CONF_USER_EMAIL=${GH_CONF_USER_EMAIL:-me@example.com}
+
+#   git config user.name "$GH_CONF_USER_NAME"
+#   git config user.email "$GH_CONF_USER_EMAIL"
+
+#   # Validate if the Git configuration was set successfully
+#   if [[ "$(git config user.name)" == "$GH_CONF_USER_NAME" && "$(git config user.email)" == "$GH_CONF_USER_EMAIL" ]]; then
+#     log_info "Git configuration set successfully for the project."
+#   else
+#     log_warning $'Failed to set Git configuration for the project.\nPlease assign the Git user.name and user.email manually using the following commands:\n\n  git config user.name "Your Name"\n  git config user.email "your.email@example.com"'
+#   fi
+
+# }
+
+# # 8.2 Validate Git LFS installation and initialization in the repository
+
+# validate_git_lfs() {
+
+#    # Check if Git LFS is installed on the local machine system
+#    if ! command -v git-lfs &> /dev/null; then
+#       log_warning $'Git LFS is not installed.\n
+# Please install Git LFS to manage large files in your repository\n
+# using git-lfs-installation-guide.'
+#       return 
+#    fi
+
+#    # Validate if Git LFS is initialized in the repository
+#    if ! git lfs install &> /dev/null; then
+#       log_warning $'Git LFS is not initialized in the repository.\n
+# Please run the following command to initialize Git LFS:\n  git lfs install'
+#       return
+#    fi
+# }
+
+# # 8.3 Validate local installation of uv (Universal Viewer)
+
+# validate_uv_installation() {
+   
+#    # Validate if uv (Astral) is installed on the local machine system
+#    if ! command -v uv &> /dev/null; then
+#       log_warning $'uv (Astral) is not installed.\n
+# Please install uv to view various file formats in your repository\n
+# using uv-installation-guide.'
+#       return
+#    fi
+
+#    # Validate if uv is installed on user-specific path /home/user/.local/bin/uv
+#    if [[ ! -f "$HOME/.local/bin/uv" ]]; then
+#       log_warning $'uv (Astral) is not installed in the user-specific path.\n
+# Please install uv in the user-specific path /home/user/.local/bin/uv\n
+# using uv-installation-guide.'
+#       return
+#    fi 
+# }
+
+# # 8.4 Set custom git config hookpath
+# set_custom_git_hookpath() {
+    
+#     # If the current git config hookpath is not equal to the desired GIT_HOOK_PATH, set it to the desired value
+#     if [[ "$(git config --get core.hooksPath)" != "$GIT_HOOK_PATH" ]]; then
+#         log_info "Setting custom git config hookpath to '$GIT_HOOK_PATH'..."
+#         git config core.hooksPath "$GIT_HOOK_PATH"
+#         log_info "Git config hookpath set to '$GIT_HOOK_PATH' successfully."
+#     else
+#         log_info "Git config hookpath is already set to '$GIT_HOOK_PATH'."
+#         return
+#     fi
+# }
+ 
+
+# # ---------------------------------------------------------
+# # 9. Main function
+# # ---------------------------------------------------------
+
+# main() {
+#     log_info "Bootstrap script started..."
+
+#     # Prompt for project name
+#     log_info "Prompting for project name..."
+#     prompt_project_name
+
+#     # Prompt for project path
+#     log_info "Prompting for project path..."
+#     prompt_project_path
+
+#     # Prompt for GitHub username
+#     log_info "Prompting for GitHub username..."
+#     prompt_github_username
+
+#     # Confirm GitHub repository existence
+#     log_info "Confirming GitHub repository existence..."
+#     confirm_github_repo_existence
+
+#     # Log the final project path
+#     log_info "Final project path: $PROJECT_PATH/$PROJECT_NAME"
+
+#     # Clone the GitHub repository
+#     log_info "Cloning the GitHub repository..."
+#     clone_github_repo
+
+#     # Validate if the project directory was created successfully
+#     if [[ -d "$PROJECT_PATH/$PROJECT_NAME" ]]; then
+#         log_info "Project directory '$PROJECT_PATH/$PROJECT_NAME' created successfully."
+#     else
+#         log_error "Failed to create project directory '$PROJECT_PATH/$PROJECT_NAME'."
+#         exit 1
+#     fi
+
+#     # Change directory to the project directory
+#     cd "$PROJECT_PATH/$PROJECT_NAME" || { log_error "Failed to change directory to '$PROJECT_PATH/$PROJECT_NAME'."; exit 1; }
+#     log_info "Changed directory to '$PROJECT_PATH/$PROJECT_NAME'."
+
+#     # Prepare the project environment
+#     log_info "Preparing the project environment..."
+#     prepare_project_environment
+
+#     # Validate Git LFS installation and initialization
+#     log_info "Validating Git LFS installation and initialization..."
+#     validate_git_lfs
+
+#     # Validate local installation of uv (Universal Viewer)
+#     log_info "Validating local installation of uv (Universal Viewer)..."
+#     validate_uv_installation
+
+#     # Set custom git config hookpath
+#     set_custom_git_hookpath
+    
+#     # Success message
+#     log_info "Bootstrap script completed successfully."
+
+# }
+
+# main "$@"
---
+++ modules/github/public-repo/bin/main-gh-public.sh
@@ -0,0 +1,218 @@
+#!/usr/bin/env bash
+
+# ============================================================
+# Purpose: This script is the main entry point for github public repo module.
+#          It orchestrates the execution of various step scripts to configure 
+#          and validate a public repository on GitHub.
+# Logic:
+#          1. Set Global Variables
+#          2. Create logging scripts
+#          3. Pre-requisites Validation
+#          4. Source the step scripts
+#          5. Execute the main function which calls the step scripts in order
+# ============================================================
+
+set -euo pipefail
+
+# ============================================================
+# 1. Set Global Variables
+# ============================================================
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+MODULE_ROOT="$(cd -- "$SCRIPT_DIR/../../" && pwd)"
+PROJECT_ROOT="$(cd -- "$SCRIPT_DIR/../../../../" && pwd)"
+SCRIPT_NAME="$(basename -- "${BASH_SOURCE[0]}")"
+
+TS=$(date +%Y-%m-%dT%H:%M:%S)
+
+# ============================================================
+# 2. Create logging scripts
+# ============================================================
+
+log_info() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;34m[INFO][$TS][$script_name] \033[0m $1"
+}
+
+log_error() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;31m[ERROR][$TS][$script_name] \033[0m $1"
+} 
+
+log_warning() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;33m[WARNING][$TS][$script_name] \033[0m $1"
+}
+
+log_debug() {
+    script_name="$(basename "${BASH_SOURCE[1]}")"
+    echo -e "\033[1;36m[DEBUG][$TS][$script_name] \033[0m $1"
+}
+
+log_info "I am child process at $MODULE_ROOT/bin/main-gh-public.sh"
+
+# ============================================================
+# 3. Pre-requisites Validation
+# ============================================================
+
+# log_info "Validating pre-requisites..."
+
+# # Validate if the 'gh' command is available
+# if command -v gh > /dev/null 2>&1; then
+#     log_info "Github cli command 'gh' found"
+# else
+#     log_error "Github cli command 'gh' not found"
+#     log_error "Use instruction 'modules/github/public-repo/instructions/github-cli-installation.md' to install the Github cli command"
+#     exit 1
+# fi
+
+# # Validate your git authentication with Github cli
+# if gh auth status > /dev/null 2>&1; then
+#     log_info "Github cli authentication is valid"
+# else
+#     log_error "Github cli authentication is invalid"
+#     log_error "Use instruction 'modules/github/public-repo/instructions/github-cli-authentication.md' to authenticate the Github cli command"
+#     exit 1
+# fi
+
+# # Validate if the 'jq' command is available
+# if command -v jq > /dev/null 2>&1; then
+#     log_info "JSON query command 'jq' found"
+# else
+#     log_error "JSON query command 'jq' not found"
+#     log_error "Use instruction 'modules/github/public-repo/instructions/jq-installation.md' to install the JSON query command"
+#     exit 1
+# fi
+
+# log_info "Pre-requisites validation completed successfully"
+
+# ============================================================
+# 4. Source the step scripts
+# ============================================================
+
+# log_info "Sourcing the step scripts..."
+
+# # 4.1 Script 01_user_prompts
+# source "${SCRIPT_DIR}/steps/01_user_prompts"
+
+# # 4.2 Script 02_repository_settings
+# source "${SCRIPT_DIR}/steps/02_repository_settings"
+
+# # 4.3 Script 03_branch_protection
+# source "${SCRIPT_DIR}/steps/03_branch_protection"
+
+# # 4.4 Script 04_security
+# source "${SCRIPT_DIR}/steps/04_security"
+
+# # 4.5 Script 05_actions_permissions
+# source "${SCRIPT_DIR}/steps/05_actions_permissions"
+
+# # 4.6 Script 06_access_control
+# source "${SCRIPT_DIR}/steps/06_access_control"
+
+# # 4.7 Script 07_labels
+# source "${SCRIPT_DIR}/steps/07_labels"
+
+# # 4.8 Script 08_project
+# source "${SCRIPT_DIR}/steps/08_project/graphql/08_project"
+
+# log_info "Step scripts sourced successfully"
+
+# ============================================================
+# 5. Main Execution
+# ============================================================
+
+# main() {
+    
+#     log_info "Starting the main execution..."
+
+#     # Script 01: User Prompts
+#     log_info "01_user_prompts: Starting user prompts..."
+#     get_github_username
+#     select_github_repositories
+#     validate_selected_repository
+#     determine_private_or_public
+#     log_info "01_user_prompts: User prompts completed successfully"
+
+#     # Script 02: Repository Settings
+#     log_info "02_repository_settings: Starting repository settings configuration..."
+#     update_repository_description
+#     configure_repository_settings
+#     enable_release_immutability
+#     log_info "02_repository_settings: Repository settings configuration completed successfully"
+
+#     # Script 03: Branch Protection
+#     log_info "03_branch_protection: Starting branch protection rules configuration and validation..."
+#     configure_main_branch_protection_rules
+#     validate_enforcement_status
+#     validate_bypass_actor
+#     is_main_only_target_branch
+#     validate_rule_restrict_creations
+#     validate_rule_restrict_updates
+#     validate_rule_restrict_deletions
+#     validate_rule_require_linear_history
+#     validate_rule_require_deployments_to_succeed
+#     validate_rule_require_signed_commits
+#     validate_rule_pull_request
+#     validate_rule_checks_to_pass
+#     validate_rule_block_force_pushes
+#     validate_rule_require_code_scanning_results
+#     validate_rule_require_code_quality_results
+#     validate_rule_restrict_code_coverage
+#     validate_rule_automatically_request_copilot_code_review
+#     log_info "03_branch_protection: Branch protection rules configuration and validation completed successfully"
+    
+#     # Script 04: Security
+#     log_info "04_security: Starting security configuration..."  
+#     enable_vulnerability_alerts
+#     enable_dependabot_security_updates
+#     enable_private_vulnerability_reporting
+#     enable_codeql_scanning
+#     log_info "04_security: Security configuration completed successfully"
+
+#     # Script 05: Actions Permissions
+#     log_info "05_actions_permissions: Starting actions permissions configuration..."
+#     enable_permission_allow_user_and_select_non_user_actions
+#     enable_permission_require_approval_for_all_external_contributors
+#     enable_permission_workflow_permissions
+#     enable_permission_artifact_and_log_retention
+#     log_info "05_actions_permissions: Actions permissions configuration completed successfully"
+    
+#     # Script 06: Access Control
+#     log_info "06_access_control: Starting access control validation..."
+#     validate_only_admin_in_collaborators_list
+#     validate_no_teams_in_teams_list
+#     log_info "06_access_controll: Access control validation completed successfully" 
+
+#     # Script 07: Labels
+#     log_info "07_labels: Starting labels configuration..."
+#     remove_existing_labels
+#     create_priority_labels
+#     create_type_labels
+#     create_area_labels
+#     create_dependabot_labels
+#     log_info "07_labels: Labels configuration completed successfully"
+
+#     # Script 08: Project
+#     log_info "08_project: Starting project creation and configuration..."
+#     create_new_project
+#     link_project_to_repository "$PROJECT_NUMBER"
+#     set_project_default_repository
+#     create_custom_field_start_date "$(get_project_id $PROJECT_NUMBER)"
+#     create_custom_field_target_date "$(get_project_id $PROJECT_NUMBER)"
+#     create_custom_field_point_estimate "$(get_project_id $PROJECT_NUMBER)"
+#     create_custom_field_sprint "$(get_project_id $PROJECT_NUMBER)"
+#     log_warning "Think to add / adjust Sprint field in term of start date sprints ,etc manually after project creation"
+#     update_view_fields_in_project_view "$PROJECT_NUMBER" \
+#                                        "Table" \
+#                                        "TABLE_LAYOUT" \
+#                                        "Title" "Assignees" "Status" "Linked pull requests" "Sub-issues progress"
+#     create_new_view_in_project "$PROJECT_NUMBER"
+#     create_new_roadmap_view_in_project "$PROJECT_NUMBER"
+#     log_info "08_project: Project creation and configuration completed successfully"
+#     log_info "Main execution completed successfully"
+#     log_warning "Ensure that all third-party packages (e.g. jq, gh) are checked if installed as part of the pre-requisites validation. If not, install them before running the script again."
+#     log_warning "Consider change manually repo name from default 'development' to 'main' after script execution if needed"
+# }
+
+# main "$@"
